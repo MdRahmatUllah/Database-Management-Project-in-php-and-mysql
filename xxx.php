8fd012018901e23b1bfbb137d048ba8e55962d0e
Md Rahmat Ullah <mdrahmatullahmaruf@gmail.com>
2021-07-21 03:30:37 +0600

    added all filed

---
+++ xxx.php
@@ -0,0 +1,116 @@
+<body>
+
+           
+                    <div class="splitter splitter-vertical file-pane-splitter" style="display: flex; flex-direction: row;">
+                        <div class="splitter-pane splitter-pane-vertical splitter-pane-auto-size" style="position: relative; height: 100%; flex: 1 1 0%;">
+                            <div class="splitter splitter-horizontal ide-splitter" style="display: flex; flex-direction: column;">
+                                <div class="splitter-pane splitter-pane-horizontal splitter-pane-auto-size" style="position: relative; width: 100%; flex: 1 1 0%;">
+                                    <div id="editingArea" class="editing-area preview-mode">
+                                        <div class="preview-frame-cover">
+                                            <div class="preview-container">
+                                                <iframe id="preview" src="http://editor.wix.com/html/editor/web/renderer/render/document/945a2f61-1b55-41db-8994-fa19c9aaf647?isEdited=true&amp;isSantaEditor=true&amp;dsOrigin=Editor1.4&amp;lang=en&amp;metaSiteId=07cef35a-3b70-4a60-a2e9-580ea4a84b0d&amp;editorSessionId=46F86862-492A-4CD7-B359-5A5B471FF87B&amp;esi=46F86862-492A-4CD7-B359-5A5B471FF87B" allowfullscreen="" __idm_id__="1025136642">
+
+                                                </iframe>
+                                            </div>
+                                        </div>
+                                        <div class="opened-panels">
+                                            <div><!-- react-empty: 28 -->
+                                            </div>
+                                        </div>
+                                        <div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="opened-panels">
+                                <div><!-- react-empty: 54 --></div>
+                            </div>
+                </div>
+                <div class="editor-controls">
+                    <div class="top-bar-wrapper">
+                        <div class="main-top-bar top-bar-lang-en">
+                            <div class="top-bar-left-section">
+                                <div class="top-bar-left-wix-logo">
+                                    <svg class="symbol symbol-wixLogo" width="78" height="32" viewBox="0 0 78 32">
+                                        <path fill="#FC0" d="M44.522 1.391c-1.53.835-2.087 2.226-2.087 5.983 0 0 .835-.696 1.948-1.113.835-.278 1.53-.835 1.948-1.113 1.252-.974 1.53-2.226 1.53-4.313 0 0-2.087 0-3.339.557z">
+
+                                        </path>
+                                        <path d="M35.478 2.365c-1.252 1.113-1.67 2.922-1.67 2.922l-4.174 16.139-3.478-13.217c-.278-1.391-.974-3.2-1.948-4.313-1.252-1.53-3.757-1.53-4.035-1.53s-2.783.139-4.035 1.53c-.974 1.113-1.53 2.922-1.948 4.313l-3.478 13.217L6.538 5.287s-.417-1.809-1.67-2.922C3.059.556-.002.835-.002.835l7.93 30.191s2.643.139 3.896-.417c1.67-.835 2.504-1.53 3.617-5.704.974-3.617 3.478-14.47 3.757-15.165.139-.417.278-1.252.974-1.252s.835.835.974 1.252c.278.835 2.922 11.548 3.757 15.165 1.113 4.174 1.809 4.73 3.617 5.704 1.252.696 3.896.417 3.896.417L40.346.835c.139 0-2.922-.278-4.87 1.53zM47.861 5.843s-.557.835-1.67 1.391c-.696.417-1.391.696-2.226 1.113-1.252.557-1.67 1.252-1.67 2.365v20.313s1.948.278 3.339-.417c1.67-.835 2.087-1.67 2.087-5.426V5.843zM67.757 16L77.914.974s-4.313-.696-6.4 1.252c-1.391 1.252-2.922 3.478-2.922 3.478l-3.757 5.426c-.139.278-.417.557-.835.557s-.557-.278-.835-.557l-3.757-5.426s-1.53-2.226-2.922-3.478c-2.087-1.948-6.4-1.252-6.4-1.252L60.243 16 50.086 31.026s4.452.557 6.539-1.391c1.391-1.252 2.643-3.2 2.643-3.2l3.757-5.426c.139-.278.417-.557.835-.557s.557.278.835.557l3.757 5.426s1.391 1.948 2.783 3.2c2.087 1.948 6.539 1.391 6.539 1.391L67.757 16z"></path></svg></div></div><div class="top-bar-center-section"><div class="top-bar-center-notification"><!-- react-text: 16990 -->You are currently in preview mode<!-- /react-text --><!-- react-text: 16991 -->
+                                        <!-- /react-text --></div>
+                                    </div>
+                                    <div class="top-bar-right-section">
+                                        <div style="position: relative; height: 100%;">
+                                            <span class="has-tooltip ">
+                                                <button class="top-bar-btn top-bar-btn-device     " data-aid="top-bar-button-switch-mode">
+                                                    <svg class="symbol symbol-topBarMobile btn-symbol " width="13" height="22" viewBox="0 0 13 22">
+                                                        <g id="Page-1" fill="none" fill-rule="evenodd"><g id="top_bar_mobile_symbol" fill="#2D4150">
+                                                            <path fill="#000" d="M11 15H2V5c0-.55.45-1 1-1h7c.55 0 1 .45 1 1v10zm0 2c0 .55-.45 1-1 1H3c-.55 0-1-.45-1-1v-1h9v1zM10 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" class="top-bar-mobile-symbol-small">
+
+                                                            </path><path fill="#000" d="M1 17h11V3.03H1V17zm11 2c0 1.1-.9 2-2 2H3c-1.1 0-2-.9-2-2v-1h11v1zM3 1h7c.83 0 1.54.5 1.84 1.23H1.16C1.46 1.5 2.16 1 3 1zm7-1H3C1.34 0 0 1.34 0 3v16c0 1.66 1.34 3 3 3h7c1.66 0 3-1.34 3-3V3c0-1.66-1.34-3-3-3z" class="top-bar-mobile-symbol-large">
+
+                                                        </path></g></g></svg><!-- react-text: 156 -->
+
+                                                        <!-- /react-text --><!-- react-text: 157 -->
+
+                                                    </button>
+                                                </span>
+                                                <div><!-- react-empty: 160 --><!-- react-empty: 161 --><!-- react-empty: 162 --><!-- react-empty: 163 --></div>
+                                            </div>
+                                            <div style="position: relative; height: 100%;">
+                                               
+                                                </div><!-- react-empty: 203 --><div class=""></div><div class="tooltips-layer"><div></div></div><div><div style="display: none;"></div></div><section class="dropdown-options select "><div class="options" tabindex="0" style=""><i class="top arrow"><svg class="symbol symbol-arrowDown" width="12" height="12" viewBox="135.1 -517.4 1024 1024"><path d="M137.3-195c0 17.4 7.4 34.7 19.8 47.1l441.4 409.1c27.3 24.8 71.9 24.8 99.2 0l438.9-429c27.3-24.8 27.3-66.9 0-91.7-27.3-24.8-71.9-24.8-99.2 0L648.1 122.3 256.3-242.2c-27.3-24.8-71.9-24.8-99.2 0-12.4 14.9-19.8 29.8-19.8 47.2z" class="c1"></path></svg></i><ol class="list" style="flex-basis: auto;"><span class="options-container"><!-- react-text: 215 -->
+                                            <!-- /react-text --><li class="option dropdown-option selected icons-dd-option"><svg class="symbol symbol-textAlignLeft" width="20" height="22" viewBox="0 0 20 22"><path d="M4 3h14v2H4zM4 15h14v2H4zM4 6h9v2H4zM4 12h9v2H4zM4 9h13v2H4z"></path></svg></li><li class="option dropdown-option icons-dd-option"><svg class="symbol symbol-textAlignCenter" width="20" height="22" viewBox="0 0 20 22"><path d="M3 3h14v2H3zM3 15h14v2H3zM6 6h8v2H6zM6 12h8v2H6zM4 9h12v2H4z"></path></svg></li><li class="option dropdown-option icons-dd-option"><svg class="symbol symbol-textAlignRight" width="20" height="22" viewBox="0 0 20 22"><path d="M4 3h14v2H4zM4 15h14v2H4zM8 6h10v2H8zM8 12h10v2H8zM5 9h13v2H5z"></path></svg></li><li class="option dropdown-option icons-dd-option"><svg class="symbol symbol-textAlignBlock" width="20" height="22" viewBox="0 0 20 22"><path d="M4 3h13v2H4zM4 15h13v2H4zM4 6h13v2H4zM4 12h13v2H4zM4 9h13v2H4z"></path></svg></li><!-- react-text: 216 -->
+                                            <!-- /react-text --></span></ol><i class="bottom arrow"><svg class="symbol symbol-arrowDown" width="12" height="12" viewBox="135.1 -517.4 1024 1024"><path d="M137.3-195c0 17.4 7.4 34.7 19.8 47.1l441.4 409.1c27.3 24.8 71.9 24.8 99.2 0l438.9-429c27.3-24.8 27.3-66.9 0-91.7-27.3-24.8-71.9-24.8-99.2 0L648.1 122.3 256.3-242.2c-27.3-24.8-71.9-24.8-99.2 0-12.4 14.9-19.8 29.8-19.8 47.2z" class="c1"></path></svg></i><div class="mouse-events-blocker"></div></div></section></div><div></div><div><link type="text/css" href="//fonts.googleapis.com/css?family=Anton:n,b,i,bi|Basic:n,b,i,bi|Caudex:n,b,i,bi|Chelsea+Market:n,b,i,bi|Corben:n,b,i,bi|EB+Garamond:n,b,i,bi|Enriqueta:n,b,i,bi|Forum:n,b,i,bi|Fredericka+the+Great:n,b,i,bi|Jockey+One:n,b,i,bi|Josefin+Slab:n,b,i,bi|Jura:n,b,i,bi|Kelly+Slab:n,b,i,bi|Marck+Script:n,b,i,bi|Lobster:n,b,i,bi|Mr+De+Haviland:n,b,i,bi|Niconne:n,b,i,bi|Noticia+Text:n,b,i,bi|Overlock:n,b,i,bi|Patrick+Hand:n,b,i,bi|Play:n,b,i,bi|Sarina:n,b,i,bi|Signika:n,b,i,bi|Spinnaker:n,b,i,bi|Monoton:n,b,i,bi|Sacramento:n,b,i,bi|Cookie:n,b,i,bi|Raleway:n,b,i,bi|Open+Sans+Condensed:300:n,b,i,bi|Amatic+SC:n,b,i,bi|Cinzel:n,b,i,bi|Sail:n,b,i,bi|Playfair+Display:n,b,i,bi|Libre+Baskerville:n,b,i,bi|Roboto:n,b,i,bi|Roboto:n,b,i,bi|Work+Sans:n,b,i,bi|Work+Sans:n,b,i,bi|Poppins:n,b,i,bi|Poppins:n,b,i,bi|Barlow:n,b,i,bi|Barlow:n,b,i,bi|Oswald:n,b,i,bi|Oswald:n,b,i,bi|Cormorant+Garamond:n,b,i,bi|Cormorant+Garamond:n,b,i,bi|Playfair+Display:n,b,i,bi|Dancing+Script:n,b,i,bi|Damion:n,b,i,bi|Suez+One:n,b,i,bi|Rozha+One:n,b,i,bi|Raleway:n,b,i,bi|Lato:n,b,i,bi|Questrial:n,b,i,bi|&amp;subset=latin-ext,cyrillic,japanese,korean,arabic,hebrew,latin" rel="stylesheet"><link type="text/css" href="https://static.parastorage.com/services/santa-resources/resources/viewer/user-site-fonts/v4/languages.css" rel="stylesheet"></div><link rel="prefetch prerender" href="https://static.parastorage.com/services/santa-editor/1.1763.13/app/external/OrganizeImagesV2/index.html">
+                                            </div>
+                                            </div>
+                                            </div>
+
+                                            <script type="text/javascript">
+                                                var santaBase = 'https://static.parastorage.com/services/santa/1.2744.15';
+                                                var editorBase = 'https://static.parastorage.com/services/santa-editor/1.1763.13';
+                                            </script>
+
+                                            <script src="https://static.parastorage.com/services/santa/1.2744.15/versions/pLoaderConfig.js"></script>
+                                            <script src="https://static.parastorage.com/services/third-party/requirejs/2.1.15/require.min.js"></script>
+                                            <script src="https://static.parastorage.com/polyfill/v2/polyfill.min.js?features=es2015,es2016,es2017,fetch&amp;flags=gated&amp;unknown=polyfill&amp;rum=0"></script>
+                                            <script src="https://static.parastorage.com/services/santa-editor/1.1763.13/app/main-r.min.js"></script>
+
+                                            <script type="text/javascript">
+                                                var _gaq = _gaq || [];
+                                                _gaq.push(['_setAccount', 'UA-2117194-1']);
+                                                _gaq.push(['_setDomainName', '.wix.com']);
+                                                _gaq.push(['_trackPageview']);
+                                                (function() {
+                                                    var ga = document.createElement('script');
+                                                    ga.type = 'text/javascript';
+                                                    ga.async = true;
+                                                    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                                                    var s = document.getElementsByTagName('script')[0];
+                                                    s.parentNode.insertBefore(ga, s);
+                                                })();
+                                            </script>
+
+
+
+                                            <script type="text/javascript">window.NREUM||(NREUM={});NREUM.info={"errorBeacon":"bam.nr-data.net","licenseKey":"c99d7f1ab0","agent":"","beacon":"bam.nr-data.net","applicationTime":37,"applicationID":"1966366,39111861","transactionName":"ZFAHNkNYXUBQVEUKXF0aNhJDUF1UclhfF0FcWQkHQxZ2V1hDXhFhVlsBB0NcQXBeWUURXF9ZABAeXFdaRQ==","queueTime":0}</script>
+                                            <script type="text/javascript">
+//<![CDATA[
+(function() {
+    var _analytics_scr = document.createElement('script');
+    _analytics_scr.type = 'text/javascript'; _analytics_scr.async = true; _analytics_scr.src = '/_Incapsula_Resource?SWJIYLWA=719d34d31c8e3a6e6fffd425f7e032f3&ns=3&cb=964949946';
+    var _analytics_elem = document.getElementsByTagName('script')[0]; _analytics_elem.parentNode.insertBefore(_analytics_scr, _analytics_elem);
+})();
+// ]]>
+</script>
+
+<script type="text/javascript" src="https://static.parastorage.com/services/wix-ck-editor/1.15.0/dist/ckeditor/ckeditor.js">
+    
+</script>
+<script type="text/javascript" src="https://static.parastorage.com/services/wix-ck-editor/1.15.0/dist/wixck.min.js">
+    
+</script>
+<iframe id="mediaGalleryFrame" src="https://editor.wix.com/_partials/media-gallery-g5/1.523.0/index.html#/init/%7B%22mediaGalleryBaseHost%22%3A%22https%3A%2F%2Feditor.wix.com%2F_partials%2Fmedia-gallery-g5%2F1.523.0%2F%22%2C%22baseHost%22%3A%22wix.com%22%2C%22i18nCode%22%3A%22en%22%2C%22windowStyle%22%3A%22fullscreen%22%2C%22i18nPrefix%22%3A%22multiple_images%22%2C%22mediaType%22%3A%22picture%22%2C%22selectionType%22%3A%22multiple%22%2C%22canShowSelectButton%22%3Atrue%2C%22canSubmitUntranscoded%22%3Afalse%2C%22disableUpload%22%3Afalse%2C%22tabsList%22%3A%5B%22private%22%2C%22external%22%2C%22public%22%2C%22shutterStock%22%5D%2C%22path%22%3A%22private%22%2C%22siteMediaToken%22%3A%22JWS.eyJraWQiOiJuRVF5Y0M5TiIsImFsZyI6IkhTMjU2In0.eyJkYXRhIjoie1widXNlcklkXCI6XCIzMDZjOGUyOS1iY2ExLTQ3NDAtYmEzZS04YTM5NTgyNmU2ODRcIixcIm1ldGFzaXRlSWRcIjpcIjA3Y2VmMzVhLTNiNzAtNGE2MC1hMmU5LTU4MGVhNGE4NGIwZFwiLFwicm9sZXNcIjpbXCJvd25lclwiXX0iLCJpYXQiOjE1MTkxOTA5NDV9.GOFnwTva60Mhauij96OX4iaSImOaEfyfBniMWxWx4hY%22%2C%22isEmailWithWixDomain%22%3Afalse%2C%22maxParallelUploads%22%3A6%2C%22translations%22%3A%7B%22header%22%3A%22Choose%20Images%22%2C%22private_media.upload_restrictions_header_message%22%3A%22Upload%20or%20drag%20and%20drop%20images%20in%20JPEG%2C%20PNG%20or%20GIF%20formats.%20Each%20file%20can%20be%20up%20to%2015MB.%22%2C%22social_images.upload_restrictions_header_message%22%3A%22Add%20images%20from%20your%20Facebook%2C%20Instagram%2C%20Dropbox%2C%20Google%20Drive%2C%20Flickr%2C%20or%20Picasa%20accounts%20at%20the%20click%20of%20a%20button.%22%2C%22free_from_wix.upload_restrictions_header_message%22%3A%22Hundreds%20of%20media%20files%20for%20you%20to%20use%20across%20your%20Wix%20sites%2C%20for%20free.%22%2C%22shutterstock_images.upload_restrictions_header_message%22%3A%22The%20Shutterstock%20collection%20offers%20high%20quality%20professional%20images%20that%20you%20can%20purchase%20and%20use%20across%20your%20Wix%20sites%2C%20at%20a%20discounted%20price.%22%2C%22uploader.upload%22%3A%22Upload%20Images%22%2C%22footer.submit%22%3A%22Add%20to%20Page%22%7D%2C%22publicMediaRoot%22%3A%22photos%22%2C%22servicesList%22%3A%5B%22facebook%22%2C%22instagram%22%2C%22dropbox%22%2C%22drive%22%2C%22flickr%22%2C%22picasa%22%5D%7D" style="border: none; display: none;">
+    
+</iframe>
+</body>
