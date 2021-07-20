e9c613bfcf79d75f8cf8962e91325fbd399cc4ac
Md Rahmat Ullah <32609767+MdRahmatUllah@users.noreply.github.com>
2021-07-21 02:48:55 +0600

    Add files via upload

---
+++ Database/game_data/data_cable_connector.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2018 at 11:45 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `game_data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_cable_connector`
+--
+
+CREATE TABLE `data_cable_connector` (
+  `img` longblob,
+  `x_name` varchar(50) DEFAULT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `price` decimal(20,6) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
